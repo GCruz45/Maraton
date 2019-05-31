a18c08643401290568c24158707c069b0704b921
Gabriel Cruz <gabrielcl45@gmail.com>
2019-05-31 12:49:11 -0500

    general fixes.

--- src/Turing.java
+++ src/Turing.java
@@ -20,9 +20,9 @@ public class Turing {
 //        }
         while (i < instructionsArray.length) {
             int c0 = 0;
-            int c1 = (tape.size() - 1) / 2;
+            int c1 = tape.size()/ 2;
             int c2 = 0;
-            if (tape.size() > 0 ){
+            if (tape.size() > 0) {//Revisa que tape no este vacio
                 c2 = tape.size() - 1;
             }
             if (instructionsArray[i] == '0') {
@@ -44,24 +44,33 @@ public class Turing {
                     i += 2;
                 }
             } else if (instructionsArray[i] == '1') {
-                if (instructionsArray[i + 1] == '0') {
-                    if (tape.size() == 0) {
-                        System.out.println("#");
-                        i += 2;
-                    } else {
-                        System.out.println(c1);
-                        System.out.println(tape.get(c1));
+//                if (tape.size() % 2 != 0) {           Este if es en el caso en el que, si la cinta tiene un numero par de elementos, la cabeza c1 deba estar en el espacio vacio que cae justo en la mitad.
+
+
+                    if (instructionsArray[i + 1] == '0') {
+                        if (tape.size() == 0) {
+                            System.out.println("#");
+                            i += 2;
+                        } else {
+                            System.out.println(tape.get(c1));
+                            i += 2;
+                        }
+                    } else if (instructionsArray[i + 1] == '1') {
+                        tape.add(c1, String.valueOf(instructionsArray[i + 2]));
+                        i += 3;
+                    } else if (instructionsArray[i + 1] == '2') {
+                        if (tape.size() != 0) {
+                            tape.remove(c1);
+                        }
                         i += 2;
                     }
-                } else if (instructionsArray[i + 1] == '1') {
-                    tape.add(c1, String.valueOf(instructionsArray[i + 2]));
-                    i += 3;
-                } else if (instructionsArray[i + 1] == '2') {
-                    if (tape.size() != 0) {
-                        tape.remove(c1);
-                    }
-                    i += 2;
-                }
+//                } else {
+//                    if (instructionsArray[i + 1] == '1') {
+//                        i += 3;
+//                    } else {
+//                        i += 2;
+//                    }
+//                }
             } else if (instructionsArray[i] == '2') {
                 if (instructionsArray[i + 1] == '0') {
                     if (tape.size() == 0) {
@@ -81,7 +90,6 @@ public class Turing {
                     i += 2;
                 }
             }
-
         }
     }
 }
