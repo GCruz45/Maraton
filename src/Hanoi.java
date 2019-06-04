16c741b8b56b98a4a614b2c5eea22e27ba5ac955
Legendary-Overlord <47962278+Legendary-Overlord@users.noreply.github.com>
2019-06-03 22:02:37 -0500

    Update Hanoi

--- src/Hanoi.java
+++ src/Hanoi.java
@@ -1,19 +1,31 @@
-import java.util.ArrayList;
-import java.util.Scanner;
-
+import java.util.*;
 public class Hanoi {
-
-
+	static int origen=0;
+    static int auxiliar=0;
+    static int destino=0;
+	
     public static void main(String[] args) {
-
-        Scanner scn = new Scanner(System.in);
-        ArrayList<Integer> columnA = new ArrayList<>();
-        ArrayList<Integer> columnB = new ArrayList<>();
-        ArrayList<Integer> columnC = new ArrayList<>();
-        int m = scn.nextInt();
-        int n = scn.nextInt();
-
-
-
+        Scanner sc = new Scanner(System.in);
+        int n;
+        System.out.println("Numero de discos: ");
+        n = sc.nextInt();
+        origen=n;
+        solveHanoi(n,origen,auxiliar,destino);  //1:origen  2:auxiliar 3:destino
     }
+
+
+//Método Torres de Hanoi Recursivo
+public static void solveHanoi(int n, int origin,  int aux, int ziel){
+  if(n==1) {
+	  origin-=1;
+	  ziel++;
+	  System.out.println(origen+"/"+auxiliar+"/"+destino);  
+  }else{
+     solveHanoi(n-1, origin, ziel, aux);
+     origin-=1;
+     ziel++;
+     System.out.println(origen+"/"+auxiliar+"/"+destino);
+     solveHanoi(n-1, aux, origin, ziel);
+   }
+ }
 }
